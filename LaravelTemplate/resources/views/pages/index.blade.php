1f1fd21a14e43e5d2522aff3fd94f04960632495
nbaluyot15@gmail.com <nbaluyot15@gmail.com>
2022-09-22 13:35:16 +0800

    Laravel Template

---
+++ LaravelTemplate/resources/views/pages/index.blade.php
@@ -0,0 +1,27 @@
+
+<div class="container p-5">
+    <div class="card bg-dark text-white p-5 w-40 m-auto">
+        <img class="card-img-top" src="{{ url('images/iremitlogo1.png') }}" alt="">
+            <!--LOGIN FORM-->
+            <div class="input-group m-1">
+                <input placeholder="Enter Username" type="text" class="form-control">
+                <div class="input-group-append">
+                    <button type="button" class="btn btn-lg btn-block bg-white">
+                        <i class="fa fa-user fa-10x "></i>
+                    </button>
+                </div>
+            </div>
+            <div class="input-group m-1">
+                <input placeholder="Enter Password" type="text" class="form-control">
+                <div class="input-group-append">
+                    <button type="button" class="btn bg-white btn-lg btn-block">
+                        <i class="fa fa-lock" aria-hidden="true"></i>
+                    </button>
+                </div>
+            </div>
+            
+            <button type="button" class="btn btn-primary btn-block m-1">LOGIN</button>
+            <!--LOGIN FORM-->
+      </div>
+    </div>
+</div>
