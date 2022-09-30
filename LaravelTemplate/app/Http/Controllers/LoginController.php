a2d5bd070f64cc34bf51b545752d80976ef914bd
nbaluyot15@gmail.com <nbaluyot15@gmail.com>
2022-09-30 15:27:39 +0800

    add notes

--- LaravelTemplate/app/Http/Controllers/LoginController.php
+++ LaravelTemplate/app/Http/Controllers/LoginController.php
@@ -3,13 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
 
 class LoginController extends Controller
 {
     public function index()
-    {
-    	return view('login');
+    {   
+        try{
+            $client = new Client();
+            $token = '';
+
+            $response = $client->request('GET', '' ,[
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json',
+                    'authorization' => 'bearer '.$token
+                ],
+                'verify' => false,
+                'http_errors' => false
+            ]);
+
+            $json = json_decode($response->getBody(),true);
+
+            return $json;
+            
+        }catch(ClientException $e){
+           return $e;
+        }
     }
+
     public function about()
     {
     	return view('about');
